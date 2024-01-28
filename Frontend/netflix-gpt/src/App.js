e553eb0d126fbdab0ff726ddaf12208fc93ed238
rishabh-gupta-au13 <rgp02071993@gmail.com>
2024-01-28 23:04:33 +0530

    statre new project react gpt

---
+++ Frontend/netflix-gpt/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-red-800">
+   <h1>This Is Netflix Gpt</h1>
+     
+     
+    </div>
+  );
+}
+
+export default App;
